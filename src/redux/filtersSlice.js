209532d60b1965ddd5a7ced838a3f61fb98d5a25
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-03-21 11:55:30 +0200

    fix

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -2,10 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const filterSlice = createSlice({
   name: 'filter',
-  initialState: '',
+  initialState: {
+    name: '',
+  },
   reducers: {
     changeFilter(state, action) {
-      return action.payload;
+      state.name = action.payload;
     },
   },
 });
@@ -14,4 +16,4 @@ export const { changeFilter } = filterSlice.actions;
 
 export default filterSlice.reducer;
 
-export const selectNameFilter = state => state.filters;
+export const selectNameFilter = state => state.filters.name;
